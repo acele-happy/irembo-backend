842fed49d43e22969b475537e07f7be71ed6e308
Dukunde Irakoze Happy <acele4444@gmail.com>
2024-02-23 13:25:21 +0200

    new

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -1,12 +1,18 @@
 const Url = require("../models/Url");
-// const { nanoid } = require("nanoid");
-const snowflake = require("snowflake-node");
+// const { nanoid } = require ("nanoid");
+const { v4: uuid4 } = require("uuid");
+// const snowflake = require("snowflake-node");
 require("dotenv").config();
+const cron = require('node-cron');
 
-const generator = new snowflake.Snowflake({
-  mid: 1, // Machine ID (unique for each server or instance)
-  offset: (2022 - 1970) * 31536000 * 1000, // Start time (milliseconds since UNIX epoch)
-});
+// const generator = new snowflake.Snowflake({
+//   mid: 1, // Machine ID (unique for each server or instance)
+//   offset: (2022 - 1970) * 31536000 * 1000, // Start time (milliseconds since UNIX epoch)
+// });
+
+exports.text = async (req, res) => {
+  return res.send("Helloo brotherr");
+};
 
 exports.shorten = async (req, res) => {
   const longUrl = req.body.longUrl;
@@ -23,7 +29,8 @@ exports.shorten = async (req, res) => {
         .status(200)
         .send({ shortUrl: `http://localhost:${process.env.PORT}/${alias}` });
     } else {
-      const uniqueAlias = generator.nextId().toString();
+      //   const uniqueAlias = generator.nextId().toString();
+      const uniqueAlias = uuid4()
       const url = await new Url({ longUrl, uniqueAlias });
       await url.save();
 
@@ -54,12 +61,12 @@ exports.redirect = async (req, res) => {
 };
 
 // Cron job to delete expired URLs every day
-cron.schedule('0 0 * * *', async () => {
-    try {
-      // Find and delete expired URLs
-      await URL.deleteMany({ expiresAt: { $lte: new Date() } });
-      console.log('Expired URLs deleted successfully');
-    } catch (err) {
-      console.error('Error deleting expired URLs:', err);
-    }
-  });
+cron.schedule("0 0 * * *", async () => {
+  try {
+    // Find and delete expired URLs
+    await URL.deleteMany({ expiresAt: { $lte: new Date() } });
+    console.log("Expired URLs deleted successfully");
+  } catch (err) {
+    console.error("Error deleting expired URLs:", err);
+  }
+});
