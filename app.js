5a61281debfce77e67efc62e7912e813d7321c6d
Dukunde Irakoze Happy <acele4444@gmail.com>
2024-02-23 23:02:42 +0200

    fix

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(userRoutes)
 app.use(urlRoutes) 
-app.get('/test', (req, res) => {
+app.get('/', (req, res) => {
     res.send('test route')
 })
 // app.use(teacherRoutes)
